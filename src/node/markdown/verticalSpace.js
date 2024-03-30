b62962ef63c80aee9c15b70a8192086cfe21b62a
James Smith <james.smith@djalbat.com>
2024-03-30 09:26:49 +0000

    Added content to vertical space.

--- src/node/markdown/verticalSpace.js
+++ src/node/markdown/verticalSpace.js
@@ -1,21 +1,28 @@
 "use strict";
 
 import MarkdownNode from "../../node/markdown";
+import contentMixins from "../../mixins/content";
 
 class VerticalSpaceMarkdownNode extends MarkdownNode {
-  asHTML(index, context) {
-    const html = null;
+  asHTML(indent, context) {
+    const content = this.content(context),
+          html = content; ///
 
     return html;
   }
 
   createDOMElement(context) {
-    const domElement = null;
+    const content = this.content(context),
+          domElement = document.createTextNode(content);
+
+    this.setDOMElement(domElement);
 
     return domElement;
   }
 
   static fromRuleNameChildNodesAndOpacity(ruleName, childNodes, opacity) { return MarkdownNode.fromRuleNameChildNodesAndOpacity(VerticalSpaceMarkdownNode, ruleName, childNodes, opacity); }
 }
+
+Object.assign(VerticalSpaceMarkdownNode.prototype, contentMixins);
 
 export default VerticalSpaceMarkdownNode;
