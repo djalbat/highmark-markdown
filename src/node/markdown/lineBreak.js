877005494278f561f006f696b0ceeb078b60c577
James Smith <james.smith@djalbat.com>
2024-04-13 19:55:26 +0100

    Fixed the line breaks.

--- src/node/markdown/lineBreak.js
+++ src/node/markdown/lineBreak.js
@@ -3,5 +3,15 @@
 import MarkdownNode from "../../node/markdown";
 
 export default class LineBreakMarkdownNode extends MarkdownNode {
+  childNodesAsHTML(indent, context) {
+    const childNodesHTML = null;
+
+    return childNodesHTML;
+  }
+
+  createChildNodeDOMElements(context) {
+    ///
+  }
+
   static fromRuleNameChildNodesAndOpacity(ruleName, childNodes, opacity) { return MarkdownNode.fromRuleNameChildNodesAndOpacity(LineBreakMarkdownNode, ruleName, childNodes, opacity); }
 }
