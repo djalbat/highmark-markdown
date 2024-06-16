cadf627963f332ce31797e7c6f024ed9abc07fd3
James Smith <james.smith@djalbat.com>
2024-06-16 16:07:42 +0100

    Corrected the BNF.

--- src/markdown/bnf.js
+++ src/markdown/bnf.js
@@ -17,7 +17,7 @@ const bnf = `
                                                    
                                                | table 
                                                    
-                                               | footnote.
+                                               | footnote
                                                    
                                                | orderedList 
                                                    
