b507c0bba0e30bc6dd3e39f7ed7da8d53c7013e3
James Smith <james.smith@djalbat.com>
2025-06-08 12:34:40 +0100

    Added the getPageNumberHTMLNode() method.

--- src/transform/html/pageNumber.js
+++ src/transform/html/pageNumber.js
@@ -4,6 +4,13 @@ import HTMLTransform from "../../transform/html"
 import PageNumberHTMLNode from "../../node/html/pageNumber";
 
 export default class PageNumberTMLTransform extends HTMLTransform {
+  getPageNumberHTMLNode() {
+    const htmlNode = this.getHTMLNode(),
+          pageNumberHTMLNode = htmlNode;  ///
+
+    return pageNumberHTMLNode;
+  }
+
   appendToDivisionHTMLNode(divisionHTMLNode) {
     const parentHTMLNode = divisionHTMLNode;  ///
 
